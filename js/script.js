905eea63f182ab9e66fcc1495827fd566d71ffb8
abobrovitskii <abobrovitskii@jaxel.com>
2023-09-23 11:58:36 -0700

    assignment 1

---
+++ js/script.js
@@ -0,0 +1,153 @@
+//STEP 1
+
+let someMonth
+function theMonth() {}
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+//STEP 2
+1
+"String"
+true
+null
+
+//STEP 3
+
+let complexExpression = 3 * 5
+let complexExpression2 = "String " + 18
+
+//STEP 4
+
+let firstName
+let strFirstName
+let lastName
+let strLastName
+let address
+let strAddress
+let city
+let strCity
+let state
+let strState
+let zipCode
+let strZipCode
+let yourAge
+let strYourAge
+let referralSource
+let strReferralSource
+let isMayWeContactYou
+let blnIsMayWeContactYou
+
+//STEP 5
+
+let city2 = "San Diego"
+let city3
+city3 = "San Diego"
+let city4 = "San Diego", zipCode1 = "92101", state1 = "CA"
+
+//STEP 6
+
+let numberAndString = "String " + 50
+console.log(numberAndString)
+
+//STEP 7
+
+let booleanAndString = true + "Boolean"
+let numberAndBoolean = 30 + true
+console.log(booleanAndString, numberAndBoolean) //trueBoolean 31
+
+//STEP 8
+
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."' //added \
+console.log(someString)
+
+//STEP 9
+
+let nullValue = null
+let undefinedValue
+console.log(nullValue, undefinedValue)
+
+//STEP 10
+
+let typeOfResult
+
+city = "San Diego"
+typeOfResult = typeof city
+console.log(typeOfResult)
+
+blnIsMayWeContactYou = true
+typeOfResult = typeof blnIsMayWeContactYou
+console.log(typeOfResult)
+
+let object = new Object()
+typeOfResult = typeof object
+console.log(typeOfResult)
+
+typeOfResult = typeof complexExpression
+console.log(typeOfResult)
+
+typeOfResult = undefinedValue
+console.log(typeOfResult)
+
+//STEP 11
+
+alert("Hello" + " Aleksand" + ", welcome to the JavaScript class!")
+
+//STEP 12
+let name = "Aleksandr"
+alert("Hello " + name + ", welcome to the JavaScript class!")
+
+//STEP 13
+
+let course = "JavaScript"
+alert("Hello " + name + ", welcome to the " + course + " class!")
+
+//STEP 14
+
+alert("Hello " + name + ". \nWelcome to the " + course + " class!")
+
+//STEP 15
+name = prompt("Whan is your name?")
+alert("Hello " + name + ". \nWelcome to the " + course + " class!")
+
+//STEP 16
+course = prompt("What class are you taking?")
+alert("Hello " + name + ". \nWelcome to the " + course + " class!")
+
+//STEP 17
+let x = 10
+let y = 20
+console.log(x + y)
+
+//STEP 18
+let x2 = 20
+x2 += 20
+console.log(x2)
+
+//STEP 19
+let x3 = 20
+x3 *= 5
+console.log(x3)
+
+//STEP 20
+let x4 = 20 % 3
+x4 /= 1
+console.log(x4)
+
+//STEP 21
+let firstOperator = prompt("Input String 1")
+let secondOperator = prompt("Input String 2")
+if (firstOperator === secondOperator) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+//STEP 22
+let firstOperator2 = prompt("Input String 1")
+let secondOperator2 = prompt("Input String 2")
+if (firstOperator2 != secondOperator2) {
+    console.log(false)
+} else {
+    console.log(true)
+}
